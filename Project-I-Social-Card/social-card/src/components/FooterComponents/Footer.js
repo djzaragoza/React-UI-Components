652a451ef1eab144f7f867c14b446d6ae8db8ad6
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-23 20:16:04 -0700

    working on footer.js file

---
+++ Project-I-Social-Card/social-card/src/components/FooterComponents/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <div className="footer";
+        <a href="" className="chat item">
+            <i class="far fa-comments fa-2x" />    
+        </a>
+        <div className="grouped item">
+            <a href="" className="load">
+                <i class="fas fa-retweet fa-2x" />
+            </a>
+            
+        </div>
+    )
+}
