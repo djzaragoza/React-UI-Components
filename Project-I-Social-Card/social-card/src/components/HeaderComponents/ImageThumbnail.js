baeb4c969c78f215aae6416981af52e0e646e792
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-24 23:43:32 -0700

    updated imagethumbnail.js file

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import './Header.css';
-import { ReactComponent as Logo } from './lambda.svg';
 
-function ImageThumbnail() {
+const ImageThumbnail = () => {
     return (
-        <div className="imageThumbnail">
-            <Logo className="logo" />
-        </div>
-    );
+        <img src="https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" alt="Lambda School crest"></img>
+    )
 }
 
 export default ImageThumbnail;
