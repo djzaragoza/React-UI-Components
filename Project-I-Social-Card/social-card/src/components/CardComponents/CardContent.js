84f3881b010c55727b29755753ed8180d958e09a
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-24 22:53:26 -0700

    updated cardcontent.js file

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,18 +1,15 @@
 import React from 'react';
 import './Card.css';
 
-function CardContent() {
+const CardContent = () => {
     return (
-        <div className="cardContent">
-            <p className="title">
-                <strong>Get Started With React</strong>
-            </p>
-            <p className="description">
-                React makes it painless to create interactive UIs.  Design simple views for each state in your application.
-            </p>
-            <p className="faint">reactjs.org</p>
+        <div className="card-content">
+            <h5><b>Get Started with React</b></h5>
+            <p className="card_content_class">React makes it painless to create interactive UIs.  Design simple views for each state in your application.</p>
+            <p className="web_address_class">react.org</p>
         </div>
-    );
+
+    )
 }
 
 export default CardContent;
