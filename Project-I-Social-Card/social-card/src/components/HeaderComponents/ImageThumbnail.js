798a7b2f36fdca859ce489491f9a8f0b50fe69d1
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-24 21:04:45 -0700

    finished imagethumbnail.js file

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+import { ReactComponent as logo } from './lambda.svg';
+
+function ImageThumbnail() {
+    return (
+        <div className="imageThumbnail">
+            <Logo className="logo" />
+        </div>
+    );
+}
+
+export default ImageThumbnail;
