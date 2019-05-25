b23b4fbc44b18d7ae82df6b97b422bbdde7a7803
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-25 00:00:30 -0700

    updated app.js

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -1,23 +1,13 @@
 import React from 'react';
 import './App.css';
-import HeaderContainer from './components/HeaderComponents/HeaderContainer';
-import CardContainer from './components/CardComponents/CardContainer';
-import Footer from './components/FooterComponents/Footer';
-import ImageThumbnail from './components/HeaderComponents/ImageThumbnail';
-import CardContent from './components/CardComponents/CardContainer';
-
+import HeaderContainer from "./components/HeaderComponents/HeaderContainer";
+import CardContainer from "./components/CardComponents/CardContainer";
 
 const App = () => {
   return (
-    <div className="master">
-      <div className="column1">
-        <ImageThumbnail />
-      </div>
-      <div className="column2">
-        <HeaderContainer />
-        <CardContainer />
-        <Footer />
-      </div>
+    <div className="app">
+      <HeaderContainer />
+      <CardContainer />
     </div>
   );
 };
