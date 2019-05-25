45bffda5dfbc2fd5da32ef0deae02ba258a4d0b3
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-24 22:43:22 -0700

    fixing cardbanner.js file

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardBanner.js
@@ -3,10 +3,12 @@ import './Card.css';
 
 const CardBanner = () => {
     return (
-        <div className="card-banner">
-            <p className="sr-only">React Logo</p>
+        <div className="banner_wrapper">
+            <div className="spacer">
+            <div className="card_banner">
+                    <img className="big-img" src="https://tk-assets.lambdaschool.com/fcd75197-7d12-46ec-bc9e-4130f34822fa_reactbackground.png" alt="React" />"
+            </div>
+            </div>
         </div>
-    );
+    )
 }
-
-export default CardBanner;
