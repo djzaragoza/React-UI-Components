c5f64f3d820d28fa6746fd25253214f0c8715917
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-27 22:00:25 -0700

    added clearbutton.js file

---
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/ClearButton.js
@@ -0,0 +1,10 @@
+//added a file called ClearButton
+
+import React from 'react';
+import './Button.css';
+
+const ClearButton = props => {
+    return <button className={props.buttonStyle}>{props.text}</button>
+};
+
+export default ClearButton;
