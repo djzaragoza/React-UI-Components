04b2a8a032c875ee8cb102066dc81948f8275350
DJ Zaragoza <zaragoza0126@gmail.com>
2019-05-27 23:46:05 -0700

    fixing app.js file

--- Project-II-Calculator/calculator/src/App.js
+++ Project-II-Calculator/calculator/src/App.js
@@ -1,14 +1,24 @@
 import React from 'react';
 import './App.css';
-import UserInputArea from './components/ButtonComponents/UserInputArea';
+
+import CalculatorDisplay from "./components/DisplayComponents/CalculatorDisplay";
+import ActionButton from "./components/ButtonComponents/ActionButton";
+import NumberButton from "./components/ButtonComponents/NumberButton";
 
 
 const App = () => {
   return (
-    <div className="app">
-      <UserInputArea />
+    <div className="calculator">
+      <CalculatorDisplay />
+      <div className="buttonSection">
+        <ActionButton text="clear" buttonStyle="actionButton" />
+        <NumberButton text="&divide;" buttonStyle="operatorButton" />
+        <NumberButton text="7" buttonStyle="numberButton" />
+        <NumberButton text="8" buttonStyle="numberButton" />
+        <NumberButton text="9" buttonStyle="numberButton"/>
+        
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
